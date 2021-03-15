3237000bde448ba66c7f5425418ec9b208af54ab
chsponciano <chs.ponciano@gmail.com>
2021-03-15 20:57:58 -0300

    repository migration

--- data/.bat
+++
@@ -1,2 +0,0 @@
-python extract.py -u http://www.cbsr.ia.ac.cn/GaitDatasetA-silh.zip -t normal -e png -l '-00_'
-python extract.py -u http://www.cbsr.ia.ac.cn/GaitDatasetB-silh.zip -t normal -e png -l '-090-'
